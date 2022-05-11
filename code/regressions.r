139585440c0bb1bd9b2759e4e86563309285c1c9
crawleyliam <liam.crawley821@gmail.com>
2022-05-11 12:40:41 -0500

    final regressions

--- code/regressions.r
+++ code/regressions.r
@@ -1,21 +1,23 @@
 
-librarian::shelf(here, tidyverse, readxl, ivprobit, stargazer, modelsummary, ivmodel)
+librarian::shelf(here, tidyverse, readxl, ivprobit, stargazer, modelsummary, ivmodel, ivreg, xtable)
 
 combined_odds <- read_csv(here("data/combined_odds.csv"))
+
 ##Models##
 
 ##probit model, no instruments, upsets based on rank
-probitMod_pts <- glm(pts_upset ~ fu_pt_difference + Series + Round,
+probitMod_pts <- glm(pts_upset ~ fu_pt_ratio + Series + Round,
                  data=combined_odds, family=binomial(link="probit"))
 
 summary(probitMod_pts)
 stargazer(probitMod_pts)
 
 ##probit model, no instruments, upsets based on betting odds
-probitMod_B365 <- glm(B365_upset ~ fu_odds_difference + Surface + Series,
-                      data=combined_odds, family=binomial(link="probit"))
+#probitMod_odds <- glm(odds_upset ~ fu_odds_difference + fu_pt_difference +Surface + Series + Round,
+#                      data=combined_odds, family=binomial(link="probit"))
 
-summary(probitMod_B365)
+#summary(probitMod_odds)
+#stargazer(probitMod_odds)
 
 
 ##first stage iv probit, using Bet365 odds as an instrument for favorite/underdog points ratio
@@ -27,8 +29,10 @@ ivProbitMod_1 <- ivprobit(pts_upset~
                             Series_ATP500+Series_Masters1000+Series_GrandSlam+Series_MastersCup+
                             fu_odds_difference,
                           data=combined_odds)
-summary(ivProbitMod_1)
-stargazer(ivProbitMod_1)
+ivProbit1_sum <- summary(ivProbitMod_1, diagnostics=TRUE)
+xtable(ivProbit1_sum)
+##Model verification
+
 
 
 ##first stage iv probit, using favorite/underdog points ratio as an instrument for favorite/underdog points ratio
@@ -38,6 +42,6 @@ ivProbitMod_2 <- ivprobit(pts_upset~
                             Series_ATP500+Series_Masters1000+Series_GrandSlam+Series_MastersCup+
                             fu_pt_difference,
                           data=combined_odds)
-summary(ivProbitMod_2)
-stargazer(ivProbitMod_2)
+ivProbit2_sum <- summary(ivProbitMod_2, diagnostics=TRUE)
+xtable(ivProbit2_sum)
 
